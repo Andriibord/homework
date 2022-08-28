b632818339b4e96476555529c78b40f9f922d85b
Andriibord <borandrii.network@gmail.com>
2022-08-28 14:49:26 +0300

    andrii

---
+++ Andrii_Bordun_DZ19/js/script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    var swiper = new Swiper(".mySwiper", {
+      cssMode: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      mousewheel: true,
+      keyboard: true,
+    });
+}
